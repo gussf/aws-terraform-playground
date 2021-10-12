1943fc95d33f191cf71c42a4a9e0fd2d2089a875
Gustavo <25396922+gussf@users.noreply.github.com>
2021-10-12 00:11:21 -0300

    Adding remote tfstate file + vpc module test

--- main.tf
+++ main.tf
@@ -4,6 +4,11 @@ terraform {
       source = "hashicorp/aws"
     }
   }
+  backend "s3" {
+    bucket = "gussf-terraform-playground"
+    key = "playground.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
@@ -11,16 +16,36 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-module "alb-ec2-test" {
-  source = "./alb-ec2-grpc"
-  providers = {
-    aws = aws
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "gussf-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
   }
-  target_instance_name = "ec2-name"
-  alb_name             = "alb-name"
-  instance_name        = "ec2-name-2"
-  environment          = "dev"
 }
+
+
+# module "alb-ec2-test" {
+#   source = "./alb-ec2-grpc"
+#   providers = {
+#     aws = aws
+#   }
+#   target_instance_name = "ec2-name"
+#   alb_name             = "alb-name"
+#   instance_name        = "ec2-name-2"
+#   environment          = "dev"
+# }
 
 /* 
 module "sqs-lambda-test" {
